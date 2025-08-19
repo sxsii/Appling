ab2251014d7665f93bc4e10ad6be2004ed19c713
Pentho Sesi Segla <sesisegla@gmail.com>
2025-08-19 15:42:46 +0100

    mid storage fix

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sysinfo::System;
+use sysinfo::{Disks, System};
 
 fn main() {
     // Initialize the System struct and collect all info
@@ -9,7 +9,7 @@ fn main() {
     let os = System::long_os_version().unwrap_or_else(|| "Unknown OS".to_string());
 
     // Get CPU brand
-    let _cpu = sys.global_cpu_info();
+    let _cpu = sys.global_cpu_usage();
     let cpu_brand = sys
         .cpus()
         .first()
@@ -17,12 +17,44 @@ fn main() {
         .unwrap_or_default();
     let cpu_display = if cpu_brand.is_empty() { "Unknown CPU".to_string() } else { cpu_brand };
 
+    // Get storage
+      /* let total_bytes: u64 = sys.disks()
+        .iter()
+        .map(|d| d.total_space())
+        .sum();
 
+    let (s_value, s_unit) = bytes_to_readable(total_bytes);
+*/
+
+println!("=> disks:");
+let disks = Disks::new_with_refreshed_list();
+for disk in &disks {
+    println!("{disk:?}");
+}
     // Total RAM in KB -> convert to GB
-    let total_ram_kb = sys.total_memory();
-    let total_ram_gb = total_ram_kb / 1024 / 1024;
+    let b_total = sys.total_memory();
+    let (r_value, r_unit) = bytes_to_readable(b_total);
+
 
     println!("Operating System: {}", os);
     println!("CPU: {}", cpu_display);
-    println!("Total RAM: {} GB", total_ram_gb);
+    println!("Total RAM: {} {}", r_value, r_unit);
+    //println!("Total Storage: {} {}", s_value, s_unit);
+}
+
+// function to see whether the memory is in TB or GB
+fn bytes_to_readable(bytes: u64) -> (u64, &'static str) {
+    let bytes_f = bytes as f64;
+    let tb = bytes_f / 1024.0 / 1024.0 / 1024.0 / 1024.0;
+
+    if tb >= 1.0 {
+        // TB case
+        let rounded = tb.round() as u64;
+        (rounded, "TB")
+    } else {
+        // GB case
+        let gb = bytes_f / 1024.0 / 1024.0 / 1024.0;
+        let rounded = gb.round() as u64;
+        (rounded, "GB")
+    }
 }
